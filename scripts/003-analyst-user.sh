79aff3f32e6f3eff4b6f716bdec7d19643dd85ca
Darren Meyer <darren@darrenpmeyer.com>
2016-05-16 00:48:57 -0400

    Generate SSH key when adding analyst

--- scripts/003-analyst-user.sh
+++ scripts/003-analyst-user.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 useradd -g users -G sudo,root,wireshark -s /bin/bash -m analyst
+sudo -u analyst ssh-keygen -q -b 4096 -f ~/.ssh/id_rsa -N ""
 echo "analyst:password" | chpasswd
 chage -d 0 analyst
